c672acf50070341ff223986c8c40c44bd0f1f317
Hafiz Muhammad Ahtasham <ahtashamjutt716@gmail.com>
2024-02-29 23:49:26 +0500

    GTA-17:Create Menu Controller

---
+++ src/main/java/com/techtide/goaltracking/controller/MenuController.java
@@ -0,0 +1,42 @@
+package com.techtide.goaltracking.controller;
+import com.techtide.goaltracking.config.SpringFXMLLoader;
+import com.techtide.goaltracking.config.StageManager;
+import com.techtide.goaltracking.enums.FxmlView;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+@Controller
+public class MenuController implements Initializable {
+    private final StageManager stageManager;
+    public MenuController(@Lazy StageManager stageManager) {
+        this.stageManager = stageManager;
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+    @FXML
+    public void onNewGoalMenuItemSelected() throws Exception {
+        stageManager.switchScene(FxmlView.NEWGOAL);
+    }
+    @FXML
+    public void onCurrentGoalMenuItemSelected() throws Exception {
+        stageManager.switchScene(FxmlView.CURRENTGOAL);
+    }
+//        @FXML
+//    public void onRecordsMenuItemSelected() throws Exception {
+//        stageManager.switchScene(FxmlView.RECORDS);    }
+//  @FXML
+//  public void onStopWatchMenuItemSelected() throws Exception {
+//        stageManager.switchScene(FxmlView.STOPWATCH);
+//  }
+//    @FXML
+//    public void onContactUsMenuItemSelected() throws Exception {      // Ye 0 code k saath exit kr de ga
+//        stageManager.switchScene(FxmlView.CONTACTUS);
+//    }
+
+}
+
+
